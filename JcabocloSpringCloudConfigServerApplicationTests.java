4f8c3956a5bc75f05eb2178c30011eff183681e6
João Caboclo da Silva Filho <36268566+JoaoCaboclo@users.noreply.github.com>
2022-04-16 08:03:01 -0300

    Add files via upload

---
+++ JcabocloSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcaboclo.microservices.jcaboclospringcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcabocloSpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
